e130baf0876d2eef8c01ec71c0ceb3a5662c7156
Anubamagpljecs07 <46440793+Anubamagpljecs07@users.noreply.github.com>
2019-01-26 16:34:22 +0530

    Create Sumszero.py

---
+++ Sumszero.py
@@ -0,0 +1,23 @@
+n=int(input())
+l=[int(i) for i in input().split()]
+y=[]
+for i in range(len(l)):
+    for j in range(len(l)):
+        if i<j :
+            if (l[i]+l[j])==0:  
+                y.append(l[i])
+                y.append(l[j])
+                i=i+1
+            elif (l[i]+l[j])==-1:
+                y.append(l[i])
+                y.append(l[j])
+                i=i+1
+            elif (l[i]+l[j])==1:
+                y.append(l[i])  
+                y.append(l[j])
+                i=i+1
+for h in range(0,len(y)):
+    if h==0:
+        print(y[h],end="")
+    else:
+        print("",y[h],end="")
