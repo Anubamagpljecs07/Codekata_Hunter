e08111d8dec347188b9e12f62b33818feb250521
Anubamagpljecs07 <46440793+Anubamagpljecs07@users.noreply.github.com>
2019-02-06 23:11:17 +0530

    Create Casefold.py

---
+++ Casefold.py
@@ -0,0 +1,12 @@
+m=input()
+s=list(m.split(" "))
+g=[]
+for i in s:
+    a=i.casefold()
+    g.append(a)
+g=sorted(g)
+for i in range(0,len(g)):
+    if i!=len(g)-1:
+        print(g[i])
+    else:
+        print(g[i])
