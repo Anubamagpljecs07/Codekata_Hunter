2440067d05abd58de6ee5d9c0545922521e26867
Anubamagpljecs07 <46440793+Anubamagpljecs07@users.noreply.github.com>
2019-02-13 19:02:57 +0530

    Update Mail.py

--- Mail.py
+++ Mail.py
@@ -1,3 +1,4 @@
+
 s=input()
 for i in range(0,len(s)):
     if s.count("@")==1 and s.count(".")==1:
@@ -6,7 +7,7 @@ for i in range(0,len(s)):
         if s[i]==".":
             b=i
             if b-a>=5 and a>=3:
-                if s[-4:-1]==".co" and s[-1]=="m":
+                if s[-4:-1]==".co" and s[-1]=="m" and s[a+1:b]=="gmail":
                     print("YES")
                     break
                 else:
